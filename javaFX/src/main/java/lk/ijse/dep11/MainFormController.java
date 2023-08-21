1c312821c59cdfa2ade306804ef0957756651915
Dewuruge <dewurugekalpadeep@gmail.com>
2023-08-21 22:55:21 +0530

    2nd commit

--- javaFX/src/main/java/lk/ijse/dep11/MainFormController.java
+++ javaFX/src/main/java/lk/ijse/dep11/MainFormController.java
@@ -1,4 +1,25 @@
 package lk.ijse.dep11;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+
 public class MainFormController {
+    public AnchorPane root;
+    public MenuItem btnNew;
+    public MenuItem btnExit;
+    public MenuItem btnUserGuide;
+    public MenuItem btnAboutUs;
+
+    public void btnNewOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnExitOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUserGuideOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAboutUsOnAction(ActionEvent actionEvent) {
+    }
 }
